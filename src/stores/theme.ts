0abfcba19ad575b9f783975501776771a5fbffc0
zhangningwei <46161524@qq.com>
2024-02-27 23:22:54 +0800

    修改默认的菜单栏宽度

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -5,7 +5,7 @@ export const useThemeStore = defineStore('todo-theme', () => {
   const headerHeight = ref(60);
   const footerHeight = ref(42);
   const showFooter = ref(true);
-  const sideMenuWidth = ref(200);
+  const sideMenuWidth = ref(256);
   const sideMenuCollapsedWidth = ref(64);
   const sideMenuCollapsed = ref(false);
   const fullScreen = ref(false);
